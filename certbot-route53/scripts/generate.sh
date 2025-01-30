cf321c4155a3edf637cf6e5e358ab1b00da103c2
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-01-30 07:59:36 -0500

    tiny updte

--- certbot-route53/scripts/generate.sh
+++ certbot-route53/scripts/generate.sh
@@ -19,16 +19,15 @@ if [ -n "$EXPAND" ]; then
 fi
 
 echo "Checking if Cert is already existing for $DOMAIN"
-if [ ! -f "/etc/letsencrypt/live/$DOMAIN//fullchain.pem" ]; then
-    echo "Processing $DOMAIN..."
-    $CERTBOT certonly --dns-route53 --non-interactive --text --agree-tos \
-        "$FORCERENEWAL" \
-        "$EXPAND" \
-        -d "$DOMAIN" \
-        "$ANDWILDCARD" \
-        --email "$EMAIL" \
-        --pre-hook "/root/certbot-route53/hook-pre.sh" \
-        --renew-hook "/root/certbot-route53/hook-each.sh" \
-        --post-hook "/root/certbot-route53/hook-post.sh" \
-        --server "https://acme-v02.api.letsencrypt.org/directory"
+if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+    echo "$DOMAIN certs are not existins hence, Processing ..."
+    echo $CERTBOT certonly --dns-route53 --non-interactive --agree-tos \
+        -m "$EMAIL" \
+        -d "$DOMAIN" "$ANDWILDCARD" -v
+    $CERTBOT certonly --dns-route53 --non-interactive --agree-tos \
+        -m "$EMAIL" \
+        -d "$DOMAIN" "$ANDWILDCARD" -v
+    # --pre-hook "/root/certbot-route53/hook-pre.sh" \
+    # --renew-hook "/root/certbot-route53/hook-each.sh" \
+    # --post-hook "/root/certbot-route53/hook-post.sh" -v
 fi
